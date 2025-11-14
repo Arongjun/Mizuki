1281d1856e9554254e69b9dccefe7d871aa67daa
Arongjun <2296488511@qq.com>
2025-11-14 15:36:33 +0800

    1

--- src/config.ts
+++ src/config.ts
@@ -21,7 +21,7 @@ const SITE_LANG = "zh_CN"; // 语言代码，例如：'en', 'zh_CN', 'ja' 等。
 const SITE_TIMEZONE =8;//设置你的网站时区 from -12 to 12 default in UTC+8
 export const siteConfig: SiteConfig = {
 	title: "晓荣讲电学",
-	subtitle: "One demo website",
+	subtitle: "xrjdx",
 	siteURL: "https://www.xrjdx.com/", // 请替换为你的站点URL，以斜杠结尾
 
 	timeZone: SITE_TIMEZONE,
